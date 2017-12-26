d2f1a6c997c44030ff0bdf6eb737062013c4ea2f
heqing <786699892@qq.com>
2017-12-26 17:30:27 +0800

    no message

---
+++ index.js
@@ -0,0 +1,10 @@
+const API = require("./lib/api_common");
+
+API.mixin(require("./lib/api_analysis"));
+API.mixin(require("./lib/api_nearby"));
+API.mixin(require("./lib/api_message"));
+API.mixin(require("./lib/api_qrcode"));
+API.mixin(require("./lib/api_template"));
+API.mixin(require("./lib/api_user"));
+
+module.exports = API;
