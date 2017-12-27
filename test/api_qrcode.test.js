7446804c7785432ca989956294f95ae8fa0cb158
heqing <786699892@qq.com>
2017-12-27 20:29:04 +0800

    no message

---
+++ test/api_qrcode.test.js
@@ -0,0 +1,7 @@
+const wxa = require("./wxa");
+const fs = require("fs");
+
+(async () => {
+  // const qrcode = await wxa.getWxaQrcodeUnlimit("welcome");
+  // qrcode.pipe(fs.createWriteStream("qrcode.jpg"));
+})();
