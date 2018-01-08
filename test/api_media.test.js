511b916e98e8ba20e897259ece41b382c1649546
heqing <786699892@qq.com>
2018-01-08 18:40:33 +0800

    media

--- test/api_media.test.js
+++ test/api_media.test.js
@@ -1,6 +1,10 @@
 const wxa = require("./wxa");
 
 (async () => {
-  console.log(await wxa.uploadImage('./images/1.png'));
-  console.log(await wxa.getMedia('pPGUCt34hSFywkvC80eYC4EHb8Oz0PnpYd9UU_VbXla5CFcFBMOzgFMqSo08VU4g'));
+  console.log(await wxa.uploadImage("./images/1.png"));
+  console.log(
+    await wxa.getMedia(
+      "pPGUCt34hSFywkvC80eYC4EHb8Oz0PnpYd9UU_VbXla5CFcFBMOzgFMqSo08VU4g"
+    )
+  );
 })();
